dc4da0cd9b17e901d7c7fe46aff34115cfee7393
RONAKON <ronakon45@gmail.com>
2022-09-30 16:45:21 +0700

    add DashboardScreen

---
+++ src/main/java/com/company/pocchartjs/screen/dashboard/DashboardScreen.java
@@ -0,0 +1,10 @@
+package com.company.pocchartjs.screen.dashboard;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("DashboardScreen")
+@UiDescriptor("dashboard-screen.xml")
+public class DashboardScreen extends Screen {
+}
